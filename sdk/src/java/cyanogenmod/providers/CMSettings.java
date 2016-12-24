6543d5ef1e390922e85eef915898921cf4a3ef43
Sam Mortimer <sam@mortimer.me.uk>
2016-12-24 13:06:05 -0800

    [2/3] Torch long press power: add auto-off function
    Change-Id: I1a9566a00a4c427d7088d19c8e4be78485dceebb

--- sdk/src/java/cyanogenmod/providers/CMSettings.java
+++ sdk/src/java/cyanogenmod/providers/CMSettings.java
@@ -1201,6 +1201,19 @@ public final class CMSettings {
                 new InclusiveIntegerRangeValidator(0, 2);
 
         /**
+         * When the torch has been turned on by long press on power,
+         * automatically turn off after a configurable number of seconds.
+         * The value is an integer number of seconds in the range 0-3600.
+         * 0 means never automatically turn off.
+         */
+        public static final String TORCH_LONG_PRESS_POWER_TIMEOUT =
+                "torch_long_press_power_timeout";
+
+        /** @hide */
+        public static final Validator TORCH_LONG_PRESS_POWER_TIMEOUT_VALIDATOR =
+                new InclusiveIntegerRangeValidator(0, 3600);
+
+        /**
          * Whether the battery light should be enabled (if hardware supports it)
          * The value is boolean (1 or 0).
          */
